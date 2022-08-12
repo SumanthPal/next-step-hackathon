146879646d872b7b609eb99383a26378601aa4d9
sumanth <sumanth@pallamreddy.com>
2022-08-11 21:19:28 -0700

    basic landing page

---
+++ app/src/components/home/home.component.jsx
@@ -0,0 +1,21 @@
+const Home = () => {
+    return (
+        <div>
+               
+            <h1>Welcome to MedObserver</h1>
+            <label>State: </label>
+            <input />
+            <button>Enter</button>
+            <div></div>
+            <label>County: </label>
+            <input />
+            <button>Enter</button>
+            
+            <footer>Created by High School Students</footer>
+
+            
+        </div>
+    )
+}
+
+export default Home;
