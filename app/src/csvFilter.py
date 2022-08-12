a8a97686e7e0c52e8a6fe74e530efb5d055db989
Rahil Pasha <rahilpasha006@gmail.com>
2022-08-12 15:40:15 -0700

    fixed directory for the csv file

--- app/src/csvFilter.py
+++ app/src/csvFilter.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-allHospitals = pd.read_csv(r"C:\Users\akhil\Desktop\next-steps-hackathon\app\src\hospitals.csv")
+allHospitals = pd.read_csv("app/src/hospitals.csv")
 # allHospitals.pop('Procedure.Hip Knee.Quality')
 # allHospitals.pop('Procedure.Hip Knee.Cost')
 # allHospitals.pop('Procedure.Pneumonia.Value')
@@ -15,5 +15,5 @@ hospitalsInState = allHospitals[allHospitals['Facility.State'].str.contains('CA'
 
 hospitalsInCity = hospitalsInState[allHospitals['Facility.City'].str.contains('Fremont')]
 
-print("\nHospitals in your city:\n",hospitalsInCity)
-print("\nMore hospitals in your state:\n",hospitalsInState)
+print("\nHospitals in your city:\n", hospitalsInCity)
+print("\nMore hospitals in your state:\n", hospitalsInState)
